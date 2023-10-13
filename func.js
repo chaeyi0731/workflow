207fefe396f3abef5a3490620be0311aeb928b78
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 13:04:08 +0900

    클릭 이벤트는 잘 적용 되었지만 if문이 적용 되지 않음

--- func.js
+++ func.js
@@ -7,13 +7,12 @@ const button = document.getElementById("button");
 for (let i = 0; i < img.length; i++) {
   img[i].addEventListener("click", () => {
     img[i].style.backgroundColor = "red";
+    const mandoo = [img[0], img[2], img[3], img[9], img[10], img[11]];
+
+    button.addEventListener("click", () => {
+      if (mandoo[0] === true) {
+        console.log("맞다");
+      }
+    });
   });
 }
-
-const mandoo = [img[0], img[2], img[3], img[9], img[10], img[11]];
-
-button.addEventListener("click", () => {
-  if (img === true) {
-    console.log(맞다);
-  }
-});
