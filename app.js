fce759c2cead3f885476fce763ed1f30c76e48f4
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 14:50:14 +0900

    html 불러오려 했으나 오류

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // 서버 파일
 const http = require("http");
 const fs = require("fs");
-
+const js = require("./func");
 const server = http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.url);
 
   res.writeHead(200, { "Contant-Type": "text/html" });
-  let doc = "<html> <head> <body> <h1> hello </h1></body> </head></html>";
-  res.end(doc);
+
+  if (req.url === "/") {
+    fs.readFile("./index.html", function (err, data) {
+      if (err) {
+        console.err("파일을 읽지 못했습니다.");
+      } else {
+        res.end(data);
+      }
+    });
+  }
 });
 server.listen(8080);
 //서버테스트 완료
