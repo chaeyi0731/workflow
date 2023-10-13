20b70a752bbc0cc80b03cb0032cd802213394277
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 15:56:02 +0900

    실험 실패

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // 서버 파일
 const http = require("http");
 const fs = require("fs");
-
+const url = require("url");
+// const func = require("func.js");
 const server = http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.url);
-
+  const pageURL = req.url;
+  const pathname = url.parse(pageURL, true).pathname;
   res.writeHead(200, { "Contant-Type": "text/html" });
 
   if (req.url === "/") {
     fs.readFile("./index.html", (err, data) => {
       if (err) {
         console.err("파일을 읽지 못했습니다.");
+      } else if (pathname === "/style.css") {
+        fs.readFile("./style.css", "utf8", function (err, data) {
+          res.writeHead(200);
+          res.write(data);
+          res.end();
+        });
       } else {
         res.end(data);
       }
