134895aa5ea8d56b7ce09bd7e1fdea0204f6e6f6
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 12:15:08 +0900

    서버 구동 테스트

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // 서버 파일
 const http = require("http");
 const fs = require("fs");
+
+http.createServer((req, res) => {
+  console.log(request.method);
+  console.log(request.url);
+
+  res.writeHead(200, { "Contant-Type": "text/html" });
+  if (request.url === "/") {
+    console.log("메인입니다.");
+  }
+});
+server.listen(8080);
