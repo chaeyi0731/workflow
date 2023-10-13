4de68c93fbc4db5a1acf2ca2daf975918948c110
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 12:51:52 +0900

    이벤트 리스너 한번에 적용이 되는지 확인

--- func.js
+++ func.js
@@ -3,3 +3,7 @@ console.dir(root);
 const img = root.children[0].children;
 console.dir(img);
 const button = document.getElementById("button");
+
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", () => {});
+}
