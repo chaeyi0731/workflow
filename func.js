4eff4d0f944370f0d2f63d53a2d25210c449a6a2
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 14:37:47 +0900

    모듈 연결 테스트

--- func.js
+++ func.js
@@ -1,3 +1,5 @@
+const { Module } = require("module");
+
 const root = document.getElementById("root");
 console.dir(root);
 const img = root.children[0].children;
@@ -7,12 +9,15 @@ const button = document.getElementById("button");
 for (let i = 0; i < img.length; i++) {
   img[i].addEventListener("click", () => {
     img[i].style.backgroundColor = "red";
-    const mandoo = [img[0], img[2], img[3], img[9], img[10], img[11]];
-
-    button.addEventListener("click", () => {
-      if (mandoo[0] === true) {
-        console.log("맞다");
-      }
-    });
   });
 }
+
+button.addEventListener("click", () => {
+  if (img[0] === true) {
+    console.log("맞다");
+  } else {
+    console.log("아니다.");
+  }
+});
+
+module.exports = func.js;
