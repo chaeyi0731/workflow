4bbb2fd959fcae72dbc5add1e93c458ed60624ab
leeeunjeong <eunjeong990731@gmail.com>
2023-10-13 17:41:48 +0900

    이미지 하나 테스트 완

--- app.js
+++ app.js
@@ -2,19 +2,21 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const path = require("path");
 // const func = require("func.js");
 const server = http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.url);
   const pageURL = req.url;
   const pathname = url.parse(pageURL, true).pathname;
-  res.writeHead(200, { "Contant-Type": "text/html" });
 
+  //* 응답받은 url이 "/"일떄
   if (req.url === "/") {
     fs.readFile("./index.html", (err, data) => {
       if (err) {
         console.err("파일을 읽지 못했습니다.");
       } else {
+        res.writeHead(200, { "Contant-Type": "text/html" });
         res.end(data);
       }
     });
@@ -25,11 +27,26 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   }
-  if (req === "/img") {
-    fs.readFile("/img", (err, data) => {
+  if (req === "/func.js") {
+    fs.readFile("/func.js", (err, data) => {
       res.writeHead(200);
       res.write(data);
       res.end();
+    });
+  }
+
+  if (req.url.startsWith("/img/")) {
+    let imageName = path.basename(req.url);
+    let imagePath = "./img/" + imageName;
+
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      res.writeHead(200, { "Contant-Type": "image/png" });
+      res.end(data);
     });
   }
 });
