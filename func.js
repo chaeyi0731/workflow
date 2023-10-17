e23744e910b73714526961bc2986a822d354efd6
leeeunjeong <eunjeong990731@gmail.com>
2023-10-17 12:27:38 +0900

    변경 사항 x

--- func.js
+++ func.js
@@ -3,18 +3,15 @@ console.dir(root);
 const img = root.children[0].children;
 console.dir(img);
 const button = document.getElementById("button");
+const mandoo = [img[0], img[2], img[3], img[9], img[10], img[11]];
 
 for (let i = 0; i < img.length; i++) {
-  img[i].addEventListener("click", function clicks() {
+  img[i].addEventListener("click", function (event) {
     img[i].style.backgroundColor = "red";
-    () => {
-      removeEventListener("click", clicks);
-    };
+    button.addEventListener("click", function () {
+      if (img[0] === event.currentTarget) {
+        console.log("o");
+      }
+    });
   });
 }
-
-button.addEventListener("click", () => {
-  if (img[0] === true) {
-    console.log("맞다");
-  }
-});
