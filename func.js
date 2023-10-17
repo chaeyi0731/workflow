0c2a690b28016189f7e333dc91bae52313446fe2
leeeunjeong <eunjeong990731@gmail.com>
2023-10-17 12:30:33 +0900

    우진님 피드백

--- func.js
+++ func.js
@@ -1,8 +1,7 @@
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 console.dir(root);
 const img = root.children[0].children;
 console.dir(img);
-<<<<<<< HEAD
 const button = document.getElementById("button");
 const mandoo = [img[0], img[2], img[3], img[9], img[10], img[11]];
 
@@ -16,24 +15,3 @@ for (let i = 0; i < img.length; i++) {
     });
   });
 }
-=======
-const button = document.getElementById('button');
-
-for (let i = 0; i < img.length; i++) {
-    img[i].addEventListener('click', function clicks() {
-        if (img[i].style.backgroundColor === '') {
-            img[i].style.backgroundColor = 'red';
-        } else if (img[i].style.backgroundColor === 'red') {
-            img[i].style.backgroundColor = '';
-        }
-    });
-}
-
-button.onclick = () => {
-    if (img[0].style.backgroundColor === 'red') {
-        alert('고기만두');
-    } else if (img[8].style.backgroundColor === 'red') {
-        alert('새우만두');
-    }
-};
->>>>>>> kimwoojin
