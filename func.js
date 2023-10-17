2f0bef337be84a1b954d31b9b4e50e33229d55a8
kimwoojin <kimwoojin1903@gmail.com>
2023-10-17 11:55:09 +0900

    간략히 수정

--- func.js
+++ func.js
@@ -1,20 +1,23 @@
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 console.dir(root);
 const img = root.children[0].children;
 console.dir(img);
-const button = document.getElementById("button");
+const button = document.getElementById('button');
 
 for (let i = 0; i < img.length; i++) {
-  img[i].addEventListener("click", function clicks() {
-    img[i].style.backgroundColor = "red";
-    () => {
-      removeEventListener("click", clicks);
-    };
-  });
+    img[i].addEventListener('click', function clicks() {
+        if (img[i].style.backgroundColor === '') {
+            img[i].style.backgroundColor = 'red';
+        } else if (img[i].style.backgroundColor === 'red') {
+            img[i].style.backgroundColor = '';
+        }
+    });
 }
 
-button.addEventListener("click", () => {
-  if (img[0] === true) {
-    console.log("맞다");
-  }
-});
+button.onclick = () => {
+    if (img[0].style.backgroundColor === 'red') {
+        alert('고기만두');
+    } else if (img[8].style.backgroundColor === 'red') {
+        alert('새우만두');
+    }
+};
