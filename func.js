00c57b4497522cc2224504c813e9985a16b14e7c
leeeunjeong <eunjeong990731@gmail.com>
2023-10-17 12:56:42 +0900

    우진님 피드백 받아 수정

--- func.js
+++ func.js
@@ -1,23 +1,39 @@
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 console.dir(root);
 const img = root.children[0].children;
 console.dir(img);
-const button = document.getElementById('button');
+const button = document.getElementById("button");
 
 for (let i = 0; i < img.length; i++) {
-    img[i].addEventListener('click', function clicks() {
-        if (img[i].style.backgroundColor === '') {
-            img[i].style.backgroundColor = 'red';
-        } else if (img[i].style.backgroundColor === 'red') {
-            img[i].style.backgroundColor = '';
-        }
-    });
+  img[i].addEventListener("click", function clicks() {
+    if (img[i].style.backgroundColor === "") {
+      img[i].style.backgroundColor = "red";
+    } else if (img[i].style.backgroundColor === "red") {
+      img[i].style.backgroundColor = "";
+    }
+  });
 }
 
 button.onclick = () => {
-    if (img[0].style.backgroundColor === 'red') {
-        alert('고기만두');
-    } else if (img[8].style.backgroundColor === 'red') {
-        alert('새우만두');
-    }
+  if (
+    img[0].style.backgroundColor === "red" &&
+    img[2].style.backgroundColor === "red" &&
+    img[2].style.backgroundColor === "red" &&
+    img[9].style.backgroundColor === "red" &&
+    img[10].style.backgroundColor === "red" &&
+    img[11].style.backgroundColor === "red"
+  ) {
+    alert("고기만두");
+  } else if (
+    img[8].style.backgroundColor === "red" &&
+    img[2].style.backgroundColor === "red" &&
+    img[2].style.backgroundColor === "red" &&
+    img[9].style.backgroundColor === "red" &&
+    img[10].style.backgroundColor === "red" &&
+    img[11].style.backgroundColor === "red"
+  ) {
+    alert("새우만두");
+  } else {
+    alert("만두 속 재료가 이상해!!");
+  }
 };
